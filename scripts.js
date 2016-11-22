d2913eda9f064137da83580d050fec933ee22182
Aesirry <jpbeltrami@gmail.com>
2016-11-22 17:17:00 +0800

    Just Kidding

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   var quote = "";
   var author = "";
-  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(data){
+  $.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function(data){
     $("#quote").html(data.quote);
     $("cite").html("- " + data.author);
     quote = data.quote;
     author = data.author;
   });
   $("#new-quote").on("click", function(){
-    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(data){
+    $.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function(data){
       $("#quote").html(data.quote);
       $("cite").html("- " + data.author);
       quote = data.quote;
