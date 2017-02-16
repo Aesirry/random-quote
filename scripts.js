ae258182a45122179729d4befc9c41e8c37c3c18
Joe Beltrami <jpbeltrami@gmail.com>
2017-02-16 15:11:24 +0800

    Fixed some aesthetic issues

--- scripts.js
+++ scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
   var quote = "";
   var author = "";
-  $.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function(data){
+
+  function getQuote() {$.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json",
+  function(data){
     $("#quote").html(data.quote);
     $("cite").html("- " + data.author);
     quote = data.quote;
     author = data.author;
-  });
-  $("#new-quote").on("click", function(){
-    $.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function(data){
-      $("#quote").html(data.quote);
-      $("cite").html("- " + data.author);
-      quote = data.quote;
-      author = data.author;
     });
-  });
+  }
+  getQuote();
+  $("#new-quote").on("click", getQuote);
   $("#tweet-button").on("click", function(){
     window.open("https://twitter.com/intent/tweet?&text=" + encodeURIComponent("\"" + quote + "\"" + "-" + author));
   });
